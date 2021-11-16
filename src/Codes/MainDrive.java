ca70893796c43ad46c5946833f329ef4a4ac265e
koolunkle <gugi329@daum.net>
2021-11-16 20:42:50 +0900

    MainDrive 클래스 작성 -> 접근 제어자(Access Modifier) 결과 확인

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -6,6 +6,14 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 
+		TestClass tc = new TestClass();
+
+		tc.pubStr = "문구"; // 정상 동작. -> public 권한 open
+
+//		tc.priStr // 접근 불가. -> private : 해당 클래스에서만 사용. 		
+
+		tc.proStr = "문구2"; // 정상 동작. -> protect 권한 OK
+
 		Animal ani = new Animal();
 
 //		ani.name protected는 남에게는 주지 않는다.
