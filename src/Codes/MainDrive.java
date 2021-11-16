a35f9a9cef8d760b3e9e4f549b68d663be7614a3
koolunkle <gugi329@daum.net>
2021-11-16 20:32:16 +0900

    MainDrive 클래스 작성 -> protected/default 결과 확인

--- src/Codes/MainDrive.java
+++ src/Codes/MainDrive.java
@@ -1,8 +1,20 @@
 package Codes;
 
+import Codes.datas.Animal;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
+
+		Animal ani = new Animal();
+
+//		ani.name protected는 남에게는 주지 않는다.
+
+//		ani.name
+
+//		ani.defStr default는 남에게는 주지 않는다.
+
+//		ani.defStr
 
 	}
 
